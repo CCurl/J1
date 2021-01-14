074647abcc7a79f8a078b34d97637f2643296bae
Chris Curl <curlbud-github@yahoo.com>
2021-01-14 12:10:00 -0500

    Start of compiler logic

--- j1-main.c
+++ j1-main.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 #define CELL_SZ 2
 #define MEM_SZ 8192
@@ -24,61 +23,123 @@ bool is_temp = false;
 bool auto_run = false;
 
 #define BUF_SZ 1024
-char input_buf[BUF_SZ];
+char tib[BUF_SZ];
+char *toIn;
 FILE *input_fp = NULL;
 FILE *input_stack[16];
 int input_SP = 0;
 int isBye = 0;
 
+#define COMMA(val) the_memory[HERE++] = (val)
+#define MAKE_LIT(val) (0x8000 | val)
+#define MAKE_JMP(to)  (0x0000 | to)
+#define MAKE_JMPZ(to) (0x2000 | to)
+#define MAKE_CALL(to) (0x4000 | to)
+#define MAKE_ALU(val) (0x6000 | val)
 
+WORD curr_op = 0;
+
+char peekChar() {
+	return *(toIn);
+}
+
+char getChar() {
+	return (*toIn) ? *(toIn++) : 0;
+}
+
+char skipWS() {
+	char ch = getChar();
+	while (ch) {
+		if (ch > 0x20) {
+			return ch;
+		} else {
+			ch = getChar();
+		}
+	}
+	return ch;
+}
+
+int getWord(char *word) {
+	char ch = skipWS();
+	int len = 0;
+	while (ch > 0x20) {
+		*(word++) = ch;
+		ch = getChar();
+	}
+	*word = 0;
+	return len;
+}
+
+int isNumber(char *word, WORD *value) {
+	*value = 0;
+	return false;
+}
+
+void parseWord(char *word) {
+	WORD num = 0;
+	if (isNumber(word, &num)) {
+		COMMA(MAKE_LIT(num));
+		return;
+	}
+	if (strcmp(word, "XXX") == 0) {
+		// do something ...
+		return;
+	}
+}
+
+void parseLine(char *line) {
+	char word[32];
+	toIn = line;
+	while (true) {
+		int len = getWord(word);
+		if (len) {
+			if (strcmp(word, "\\") == 0) { return; }
+			if (strcmp(word, "//") == 0) { return; }
+			parseWord(word);
+		} else {
+			return;
+		}
+	}
+}
+
+void getLine(char *line) {
+}
+
+void doCompile() {
+	COMMA(MAKE_LIT(0xFFFF));
+	COMMA(MAKE_LIT(4));
+	COMMA(MAKE_LIT(0));
+	COMMA(MAKE_JMPZ(1));
+	COMMA(MAKE_JMP(0));
+}
+
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
 // ---------------------------------------------------------------------
 void parse_arg(char *arg) 
 {
 	// -f:baseFn
 	if (*arg == 'f') strcpy(base_fn, arg+2);
 
-	// -b (bootstrap)
-	if (*arg == 'b') run_saved = false;
-
-	// -t (is-temp)
-	if (*arg == 't') is_temp = true;
-
 	// -a (auto-run)
 	if (*arg == 'a') auto_run = true;
 
-	if (*arg == '?')
-	{
-		printf("usage: mforth [options]\n");
-		printf("\t-f:baseFn (default: 'mforth')\n");
-		printf("\t-b        (bootstrap, default: false)\n");
-		printf("\t-t        (is-temp,   default: false)\n");
-		printf("\t-a        (auto-run,  default: false)\n");
+	if (*arg == '?') {
+		printf("usage: j1 [options]\n");
+		printf("\t-f:baseFn (default: 'j1')\n");
 		printf("\nNotes ...");
-		
 		printf("\n\n    -f:baseFn defines the base filename for the files in the working set.");
-		printf("\nThis is used by the other options.");
 
-		printf("\n\n    -b tells mforth to start with an empty dictionary. Then mforth");
-		printf("\nreads the <baseFn>.SRC file as initial input. You can define an empty");
-		printf("\n<baseFn>.SRC file to drop into the REPL with no words at all. I have");
-		printf("\nprovided the empty.SRC file for that purpose. Use 'mforth -f:empty -b'");
-
-		printf("\n\n    -t tells mforth that it should not save its state when closing. By");
-		printf("\ndefault, on exit (bye), mforth will create a set of files <baseFn>.[TXT|INF|BIN]");
-		printf("\nthat allow mforth to remember the current dictionary the next time it is run.");
-
-		printf("\n\n    -a tells mforth to automatically run the last word in the working set");
-		printf("\ndictionary and then exit. In this case, -t is automatically set to true. One would");
-		printf("\nuse this option to run a stand-alone program.");
 		exit(0);
 	}
 }
 
-// ---------------------------------------------------------------------
-// ---------------------------------------------------------------------
-// ---------------------------------------------------------------------
 
-
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
 // ---------------------------------------------------------------------
 // ---------------------------------------------------------------------
 // ---------------------------------------------------------------------
@@ -86,19 +147,15 @@ int main (int argc, char **argv)
 {
 	strcpy(base_fn, "j1");
 
-    for (int i = 1; i < argc; i++)
-    {
-        char *cp = argv[i];
-        if (*cp == '-') parse_arg(++cp);
-    }
+	for (int i = 1; i < argc; i++) {
+		char *cp = argv[i];
+		if (*cp == '-') { parse_arg(++cp); }
+	}
 
 	j1_init();
-	the_memory[HERE++] = 0xFFFF;
-	the_memory[HERE++] = 0x8003;
-	the_memory[HERE++] = 0x8001;
-	the_memory[HERE++] = 0x2001;
+	doCompile();
 	j1_emu(0);
 
 	printf("\nj1 exiting");
-    return 0;
+	return 0;
 }
